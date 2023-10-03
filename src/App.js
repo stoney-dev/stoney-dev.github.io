98b95f36c5216641d591700ef55818350dd7851f
supernova <seungmin10524@gmail.com>
2023-10-03 10:15:40 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className='title1'>There is no limit.</p>
+        <p className='title2'>개발자를 위한 최고의 커뮤니티를 제공합니다</p>
         <a
-          className="App-link"
+          className="discord"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          discord
         </a>
       </header>
     </div>
